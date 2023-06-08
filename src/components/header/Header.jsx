fe3223ec358521778175ac1410ecdefe669ebc25
YaakovSahale <yaakov.sahale@gmail.com>
2023-06-08 21:57:13 +0300

    heder complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,19 @@
 import styles from "./header.module.css";
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerTitles}>
+        <span className={styles.headerTitleSm}>React & Node</span>
+        <span className={styles.headerTitleLg}>Blog</span>
+      </div>
+      <img
+        className={styles.headerImg}
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Header;
