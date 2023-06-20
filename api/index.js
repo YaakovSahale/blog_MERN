8f8d0d2d4d232594b8a48c80a7476ddffef6ee1e
YaakovSahale <yaakov.sahale@gmail.com>
2023-06-20 17:52:46 +0300

    base module and routes and first doc in DB

--- api/index.js
+++ api/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const cors = require("cors");
+
 dotenv.config();
+app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("connected to blogDB"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("server is running on port 5000");
