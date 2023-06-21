3441365e0703afb07a05e81b15aa807ad296baa5
YaakovSahale <yaakov.sahale@gmail.com>
2023-06-21 19:32:23 +0300

    GET user api FIN

--- api/routes/users.js
+++ api/routes/users.js
@@ -56,7 +56,9 @@ router.delete("/:id", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
