db0b098503e065fb1f1e721a1bde8faf929ae42b
YaakovSahale <yaakov.sahale@gmail.com>
2023-06-22 22:41:56 +0300

    upload pic by multer FIN

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 const cors = require("cors");
 
 dotenv.config();
@@ -16,6 +17,20 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("connected to blogDB"))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "hello.jpg");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("file has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
