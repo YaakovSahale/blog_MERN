d596e836fcf5920ecfda4cd1de4966c291821e02
YaakovSahale <yaakov.sahale@gmail.com>
2023-06-26 21:25:31 +0300

    building Reducer & context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ContextProvider } from "./context/Context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <ContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
+  </React.StrictMode>
 );
--- client/src/screens/login/Login.jsx
+++ client/src/screens/login/Login.jsx
@@ -1,19 +1,49 @@
 import { NavLink } from "react-router-dom";
+import { useContext, useRef } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 import styles from "./login.module.css";
 
 const Login = () => {
+  const { user, dispatch, isFetching } = useContext(Context);
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const API_URL = "http://localhost:5000/api";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const { data } = await axios.post(`${API_URL}/auth/login`, {
+        userName: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+  console.log(user);
   return (
     <div className={styles.loginWarper}>
       <span className={styles.title}>Login</span>
-      <form className={styles.form}>
-        <label>Email</label>
-        <input type="text" placeholder="Enter your email" />
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label>UserName</label>
+        <input type="text" placeholder="Enter your UserName" ref={userRef} />
         <label>Password</label>
-        <input type="Password" placeholder="Enter your password" />
-        <button className={styles.loginBtn}>Login</button>
+        <input
+          type="Password"
+          placeholder="Enter your password"
+          ref={passwordRef}
+        />
+        <button className={styles.loginBtn} type="submit">
+          Login
+        </button>
       </form>
       <button className={styles.registerBtn}>
-        <NavLink className="link" to="/register">Register</NavLink>
+        <NavLink className="link" to="/register">
+          Register
+        </NavLink>
       </button>
     </div>
   );
