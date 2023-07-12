bc7d103aab2f40866a06d1bafbccb6644e514d90
YaakovSahale <yaakov.sahale@gmail.com>
2023-07-12 19:10:20 +0300

    showing categories in post card

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,35 +2,29 @@ import styles from "./post.module.css";
 import { Link, NavLink } from "react-router-dom";
 
 const Post = ({ post }) => {
-  const IMAGES_URL = "http://localhost:5000/images";
+  const IMAGES_URL = "http://localhost:5000/images/";
+
   return (
     <div className={styles.post}>
-      <img
-        className={styles.postImg}
-        src={`${IMAGES_URL}/${post.photo}`}
-        alt=""
-      />
-
+      {post.photo && (
+        <img className={styles.postImg} src={IMAGES_URL + post.photo} alt="" />
+      )}
       <div className={styles.postInfo}>
         <div className={styles.postCats}>
-          {post.categories?.map((cat, i) => (
-            <Link key={i} className="link" to={`/?category=${cat}`}>
-              <span className={styles.postCat} key={i}>
-                {cat}
-              </span>
+          {post.categories.map((cat, i) => (
+            <Link className="link" to={`/?category=${cat}`} key={i}>
+              <span className={styles.postCat}>{cat}</span>
             </Link>
           ))}
         </div>
-        <Link className={styles.postTitle} to={`/post/${post._id}`}>
-          <span className={styles.postTitle}>{post.title} </span>
+        <Link to={`/post/${post._id}`} className={styles.postTitle}>
+          <span className={styles.postTitle}>{post.title}</span>
         </Link>
-
         <hr />
         <span className={styles.postData}>
           {new Date(post.createdAt).toDateString()}
         </span>
       </div>
-
       <p className={styles.postDesc}>{post.desc}</p>
     </div>
   );
